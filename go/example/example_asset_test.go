54bc83277c75e9d2c4ec37b5ad36af1e897ad5a8
rot1024 <aayhrot@gmail.com>
2025-01-24 00:48:23 +0900

    chore(gp): add examples

--- go/example/example_asset_test.go
+++ go/example/example_asset_test.go
@@ -6,9 +6,57 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"strings"
 
 	cms "github.com/reearth/reearth-cms-api/go"
 )
+
+func ExampleCMS_UploadAssetDirectly_normal() {
+	ctx := context.Background()
+	c, err := cms.New(cmsURL, cmsToken)
+	if err != nil {
+		panic(err)
+	}
+
+	// upload asset
+	body := strings.NewReader("test!")
+	assetID, err := c.UploadAssetDirectly(ctx, cmsProject, "test.txt", body)
+	if err != nil {
+		panic(fmt.Errorf("failed to upload asset: %w", err))
+	}
+
+	// read asset
+	asset, err := c.Asset(ctx, assetID)
+	if err != nil {
+		panic(err)
+	}
+
+	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, asset.URL, nil)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		panic(fmt.Errorf("unexpected status code: %d", resp.StatusCode))
+	}
+	if resp.Header.Get("Content-Encoding") != "" {
+		panic(fmt.Errorf("unexpected content encoding: %s", resp.Header.Get("Content-Encoding")))
+	}
+
+	buf := new(bytes.Buffer)
+	_, err = buf.ReadFrom(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Asset Content: %s\n", buf.String())
+
+	// Output:
+	// Asset Content: test!
+}
 
 func ExampleCMS_UploadAssetDirectly_gzip() {
 	ctx := context.Background()
@@ -68,6 +116,59 @@ func ExampleCMS_UploadAssetDirectly_gzip() {
 
 	// Output:
 	// Asset Content: test
+}
+
+func ExampleCMS_CreateAssetUpload_normal() {
+	ctx := context.Background()
+	c, err := cms.New(cmsURL, cmsToken)
+	if err != nil {
+		panic(err)
+	}
+
+	// upload asset
+	body := strings.NewReader("test!")
+
+	upload, err := c.CreateAssetUpload(ctx, cmsProject, "test.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := c.UploadToAssetUpload(ctx, upload, body); err != nil {
+		panic(err)
+	}
+
+	asset, err := c.CreateAssetByToken(ctx, cmsProject, upload.Token)
+	if err != nil {
+		panic(err)
+	}
+
+	// read asset
+	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, asset.URL, nil)
+	req.Header.Set("Accept-Encoding", "gzip")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		panic(fmt.Errorf("unexpected status code: %d", resp.StatusCode))
+	}
+	if resp.Header.Get("Content-Encoding") != "" {
+		panic(fmt.Errorf("unexpected content encoding: %s", resp.Header.Get("Content-Encoding")))
+	}
+
+	buf := new(bytes.Buffer)
+	_, err = buf.ReadFrom(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Asset Content: %s\n", buf.String())
+
+	// Output:
+	// Asset Content: test!
 }
 
 func ExampleCMS_CreateAssetUpload_gzip() {
